2570b4c3e27335ad045a0a1ce670a54f255cfbdc
Giancarlo Tomasello <gtomasel20@gmail.com>
2025-10-21 22:08:34 -0700

    Created the sample shop page to display items

---
+++ ProjectAlchemy-Front/src/Banner.jsx
@@ -0,0 +1,9 @@
+function Banner(){
+    return(
+        <div className="banner">
+            <img className="banner-img" src="https://placehold.co/800x200" alt="banner img"></img>
+            <h1 className="banner-title">Shop Title</h1>
+        </div>
+    );
+}
+export default Banner
---
+++ ProjectAlchemy-Front/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-img" src="https://placehold.co/150" alt="item img"></img>
+            <h2 className="card-title">Item Name</h2>
+            <p className="card-text">Item description goes here</p>
+        </div>
+    );
+}
+export default Card
