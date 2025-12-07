0364faa294dcb115c4f7a26bd8ad44180b466c7b
Giancarlo Tomasello <gtomasel20@gmail.com>
2025-12-07 12:49:39 -0800

    updated back end to fix missing prices for equipment and assign default prices for magic items

--- ProjectAlchemy-Server/store.dto.js
+++ ProjectAlchemy-Server/store.dto.js
@@ -1,10 +1,11 @@
 class CreateStoreDTO{
-    constructor(id, name, type, rarity, cost, equipmentCatagory, gearCategory, description, weight){
+    constructor(id, name, type, rarity, cost, unit, equipmentCatagory, gearCategory, description, weight){
         this.id=id;
         this.name=name;
         this.type=type;
         this.rarity=rarity;
-        this.cost=cost;
+        this.cost=this.calculateCost(cost, rarity);
+        this.unit=unit;
         this.equipmentCatagory=equipmentCatagory;
         this.gearCategory=gearCategory;
         this.description=description;
@@ -13,6 +14,33 @@ class CreateStoreDTO{
 
     isValid(){
         return this.name && this.id && this.rarity;
+    }
+
+    calculateCost(cost, rarity){
+        if(rarity=='basic' && cost > 0){
+            return cost
+        } else if (rarity=='basic' && cost <= 0){
+            return 1
+        } else if(rarity!='basic' && cost < 0){
+            switch (rarity){
+                case 'Common':
+                    return '100';
+                case 'Uncommon':
+                    return '400';
+                case 'Rare':
+                    return '4000';
+                case 'Very Rare':
+                    return '40000';
+                case 'Legendary':
+                    return '200000';
+                case 'Artifact':
+                    return 'Priceless';
+                default:
+                    return cost;
+            }
+        }else {
+            return cost;
+        }
     }
 }
 
